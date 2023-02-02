6268ae286f4a2b3b817b7e48fceead6b4c978091
Sadia1505076 <1505076.st@ugrad.cse.buet.ac.bd>
2023-02-02 20:48:16 +0600

    time and memory optimized

--- binary_level_order_traversal.cpp
+++ binary_level_order_traversal.cpp
@@ -15,47 +15,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-struct TreeNodeWithLevel {
-    TreeNode* treeNode;
-    int level;
-};
 
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> traversal;
-        queue<TreeNodeWithLevel*> bfs_queue;
-        vector<int> levelValue;
-        int levelIndicator = 1;
-        TreeNodeWithLevel* rootWithLevel = new TreeNodeWithLevel();
-        rootWithLevel -> treeNode = root;
-        rootWithLevel -> level = 1;
-        bfs_queue.push(rootWithLevel);
+        queue<TreeNode*> bfs_queue;
+        vector<int> nodeValue;
+
+        TreeNode* emptyTreeNode = new TreeNode(-9999);
+        bfs_queue.push(root);
+        bfs_queue.push(emptyTreeNode);
 
         while(!bfs_queue.empty()) {
-            TreeNodeWithLevel* front = bfs_queue.front();
-            if (front -> level == levelIndicator) {
-                levelValue.push_back(front -> treeNode -> val);
+            TreeNode* front = bfs_queue.front();
+            if (front != NULL) {
+                nodeValue.push_back(front -> val);
+                if (front -> left != NULL) {
+                    bfs_queue.push(front -> left);
+                }
+                if (front -> right != NULL) {
+                    bfs_queue.push(front -> right);
+                }
+                bfs_queue.pop();
+                if (!bfs_queue.empty() && bfs_queue.front() != NULL && bfs_queue.front() -> val == -9999) {
+                    traversal.push_back(nodeValue);
+                    nodeValue.clear();
+                    TreeNode* emptyTreeNode = new TreeNode(-9999);
+                    bfs_queue.push(emptyTreeNode);
+                    bfs_queue.pop();
+                }
             }
-            else {
-                traversal.push_back(levelValue);
-                levelIndicator++;
-                levelValue.clear();
-                levelValue.push_back(front -> treeNode -> val);
-            }
-
-            TreeNodeWithLevel* childTreeNode = new TreeNodeWithLevel();
-            if (front -> treeNode -> left != NULL) {
-                childTreeNode -> treeNode = front -> treeNode -> left; 
-                childTreeNode -> level = front -> level + 1;
-                bfs_queue.push(childTreeNode);
-            }
-
-            if (front -> treeNode -> right != NULL) {
-                childTreeNode -> treeNode = front -> treeNode -> right;
-                childTreeNode -> level = front -> level + 1;
-                bfs_queue.push(childTreeNode);
-            }
+            else bfs_queue.pop();
         }
         return traversal;
     }
