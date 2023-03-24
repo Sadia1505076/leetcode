9aefc2cdfb5d026b72eba909401f8974fe42ff36
Sadia1505076 <1505076.st@ugrad.cse.buet.ac.bd>
2023-03-24 15:16:53 +0600

    invert binary tree with bfs

---
+++ invert_binary_tree.cpp
@@ -0,0 +1,50 @@
+// Given the root of a binary tree, invert the tree, and return its root.
+
+// Example 1:
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        queue<TreeNode*> visited;
+        visited.push(root);
+        while (!visited.empty()) {
+            TreeNode* front = visited.front();
+            visited.pop();
+            if (front != NULL) {
+                if (front -> left != NULL && front -> right != NULL) {
+                    TreeNode* temp = front->left;
+                    front->left = front -> right;
+                    front -> right = temp;
+                    visited.push(front -> left);
+                    visited.push(front -> right);
+                }
+                else if (front -> left == NULL && front -> right != NULL) {
+                    front -> left = front -> right;
+                    front -> right = NULL;
+                    visited.push(front->left);
+                }
+                else {
+                    front -> right = front -> left;
+                    front->left = NULL;
+                    visited.push(front->right);
+                }
+            }
+        }
+        return root;
+    }
+};
